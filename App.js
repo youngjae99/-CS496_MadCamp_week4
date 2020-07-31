d287ddcabfcaa137baea654ef3fe9eb92a49d9a1
youngjae <elite1111cyj@gmail.com>
2020-08-01 02:55:24 +0900

    2

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ function DetailsScreen({navigation}) {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
       <Button
-        title="Go to Details... again"
+        title="Go to Details...again"
         onPress={() => navigation.push('Details')}
       />
     </View>
