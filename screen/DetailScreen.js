98ffbddf9198aea3b4a27cffe5d96f0983355a8f
youngjae <elite1111cyj@gmail.com>
2020-08-01 02:52:05 +0900

    first commit

---
+++ screen/DetailScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function DetailsScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+}
